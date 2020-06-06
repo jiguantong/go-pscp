70ed89d988ea02adef3fec4d74feb3eaa512ce7d
04637 <04637@163.com>
2020-06-06 11:10:00 +0800

    v0.7

--- main.go
+++ main.go
@@ -6,25 +6,40 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
-	viper.SetConfigName("config")
+	viper.SetConfigName("pscp")
 	viper.AddConfigPath(".")
 	if err := viper.ReadInConfig(); err != nil {
-		log.Println("配置文件读取异常")
+		log.Println("配置文件读取异常: " + err.Error())
 		os.Exit(1)
 	}
+	var puttyPath string
+	path := strings.Split(os.Getenv("PATH"), ";")
+	for _, v := range path {
+		if strings.Contains(v, "PuTTY") {
+			puttyPath = v
+			break
+		}
+	}
+	if len(puttyPath) == 0 {
+		log.Printf("PATH 中未找到 PuTTY, 请先安装 PuTTY\ndownload: %s\n", "https://the.earth.li/~sgtatham/putty/0.73/w64/putty-64bit-0.73-installer.msi")
+		os.Exit(1)
+	}
+
 	localDir := viper.GetString("local.dir")
 	host := new(Host)
 	host.Dir = viper.GetString("host.dir")
 	host.User = "root"
 	host.Ip = viper.GetString("host.ip")
 	host.Pwd = viper.GetString("host.password")
-	fmt.Println(host)
-	fmt.Println("localDir: " + localDir)
-	cl := "pscp -r -pw " + host.Pwd + " " + localDir + " " + host.User + "@" + host.Ip + ":" + host.Dir
-	cmd := exec.Command("cmd", "/C", cl)
+	cmd := exec.Command(puttyPath+"pscp.exe", "-r", "-pw",
+		host.Pwd, localDir,
+		host.User+"@"+host.Ip+":"+
+			host.Dir)
+	fmt.Println(cmd.Args)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
